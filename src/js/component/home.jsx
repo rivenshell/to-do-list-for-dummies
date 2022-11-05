ab777cbf7249fc70d692cce1198df1adaf5341c8
Riv <rivenshell111@gmail.com>
2022-11-05 00:20:35 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -27,14 +27,82 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	
+	// Task (list state)
+	// Main state storing tasks
+	const [toDo, setToDo] = useState([
+		{"id": 1, "title": "Task 1", "status": false},
+		{"id": 2, "title": "Task 2", "status": false}
+	])
+
+	// State
+	const [newTask, setNewTask] = useState('');
+	const [updateData, setupdateData] = useState('');
+
+	//Add task
+	//////////////////////
+	const addTask = () => {
+		//
+	}
+	//Deletetask
+	//////////////////////
+	const deletetask = (id) => {
+		//
+	}
+	//mark task as done
+	//////////////////////
+	const markdone = (id) => {
+		//
+	}
+	// cancel update
+	//////////////////////
+	const cancelupdate = (id) => {
+		//
+	}
+	// change task for update
+	//////////////////////
+	const changetask = (id) => {
+		//
+	}
+	// update task
+	//////////////////////
+	const updatetask = (id) => {
+		//
+	}
+
+
 
 	return (
 		<div className="container">
-			<FontAwesomeIcon icon={faCircleCheck} />
+			<FontAwesomeIcon icon="fa-solid fa-x" />
 			<br /><br />
 		    <h2>To Do List App (ReactJS)</h2>
 			<br /><br />
-			There are no tasks selected
+			{/*Display ToDos*/}
+
+			{toDo && toDo.length ? '' : 'No tasks..'}
+			{toDo && toDo
+			  .map((task, index) => {
+				return(
+					<React.Fragment key={task.id}>
+						<div className="col taskBg">
+
+						<div className={task.status ? 'done' : ''}>
+						 <span className= "taskNumber">{index + 1}</span>
+						 <span className= "taskText">{task.title}</span>		
+						</div>
+							<div className="iconsWarp"> 
+							<span>
+							<FontAwesomeIcon icon="fa-solid fa-x" />
+							</span>
+							<span></span>
+							<span></span>
+							</div>
+						</div>
+					</React.Fragment>
+				)
+			  })
+			}
+
 		</div>
 	);
 };
